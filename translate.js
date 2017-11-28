22ca500934e61ccce12f6f070b16ccafe43492ff
小兴 <liuxing87327@163.com>
2017-11-29 00:37:00 +0800

    Update translate.js
    直接用translation.js进行翻译，解决大陆谷歌不可用问题
    简化的翻译结果的提取

--- translate.js
+++ translate.js
@@ -1,42 +1,37 @@
 #!./node
-var translate = require('google-translate-api');
 
-var source = process.argv[2]
-var target = process.argv[3]
-var query = process.argv[4]
-var target_display_name = process.argv[5]
+var tjs = require('translation.js');
 
-translate(query, { from: source, to: target, raw: true }).then(res => {
-  var rawStr = res.raw.replace(/,,/g, ',null,').replace(/,,/g, ',null,').replace(/\[,/g, '[null,');
-  var raw = JSON.parse(rawStr);
+var source = process.argv[2] || 'auto';
+var target = process.argv[3] || 'auto';
+var query = process.argv[4] || '';
+var target_display_name = process.argv[5];
 
-  output = { items: [] };
+tjs.translate({
+		text: query,
+		from: source,
+		to: target
+	})
+	.then(res => {
+		// console.log(JSON.stringify(res, null, "\t"));
 
-  if (res.from.text.autoCorrected) {
-    var autoCorrected = res.from.text.value.replace(/\<[^\<\>]+\>/g, '');
-    autoCorrected = autoCorrected.replace(/\[/, '').replace(/\]/, '');
-    output.items.push({ title: autoCorrected, subtitle: 'Did you mean this?', autocomplete: autoCorrected });
-  }
+		var output = {
+			items: []
+		};
 
-  if (res.text) {
-    const text = res.text.replace(/null$/, '');
-    output.items.push({ title: text, subtitle: target_display_name, arg: text });
-  }
+		if (res.result) {
+			res.result.forEach((data) => {
+				if (data !== query) {
+					output.items.push({
+						title: data,
+						subtitle: query,
+						arg: data
+					});
+				}
+			});
+		}
 
-  if (raw && raw[1]) {
-    raw[1].forEach((group) => {
-      var type = group[0];
-
-      group[2].forEach((wordData) => {
-        var word = wordData[0];
-        var translations = wordData[1];
-        var frequency = wordData[3];
-        output.items.push({ title: word, subtitle: `(${type}) ${translations.join(', ')}`, arg: word });
-      });
-    });
-  }
-
-  console.log(JSON.stringify(output));
-}).catch(e => {
-  console.error(e);
-});
+		// console.log(JSON.stringify(output));
+	}).catch(e => {
+		console.error(e);
+	});
